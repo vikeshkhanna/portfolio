55a0c37ff26600079008b92b9782a30d8ef2cdf5
Vikesh <khanna.vikesh@gmail.com>
2012-11-12 02:26:23 +0530

    Upadting music app

---
+++ music/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+def get_artist_pic_folder(instance, filename):
+	return "music/artists/" + instance.resolved_name() + "/" + filename
+
+def get_song_folder(instance, filename):	
+	return "music/songs/" + instance.artist.resolved_name() + "/" + instance.album.resolved_name() + "/" + filename
+
+def get_album_folder(instance, filename):
+	return "music/artists/" + instance.resolve_name() + "/" + filename
+
+class Song(models.Model):
+	title = models.CharField(max_length=100)
+	artist = models.ForeignKey('music.Artist')
+	album = models.ForeignKey('music.Album')
+
+	def __unicode__(self):
+		return self.title 
+
+class Artist(models.Model):
+	name = models.CharField(max_length=100)
+	pic_small = models.ImageField(upload_to=get_artist_pic_folder, blank=True, null=True)
+	pic_large = models.ImageField(upload_to=get_artist_pic_folder, blank=True, null=True)
+	
+	def __unicode__(self):
+		return self.name
+
+	def resolved_name(self):
+		return self.name.replace(" ","_") + "_" + str(self.id)
+
+class Album(models.Model):
+	title = models.CharField(max_length=100)
+	cover = models.ImageField(upload_to=get_album_folder, blank=True, null=True)
+	
+	def __unicode__(self):
+		return self.title
+	
+	def resolved_name(self):
+		return self.title.replace(" ","_") + "_" + str(self.id)
+
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,9 +15,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': 'portfolio',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'vikeshkhanna',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -120,6 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',  
+    'music',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
